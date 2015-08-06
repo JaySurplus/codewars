a03129db6958804aeb5e28e8d0caa7ec622bc733
JaySurplus <znznbest2004@gmail.com>
2015-08-06 12:30:43 -0500

    Starting on where my anagrams

--- going_to_zero_or_inf.py
+++ going_to_zero_or_inf.py
@@ -4,15 +4,7 @@ http://www.codewars.com/kata/55a29405bc7d2efaff00007c/train/python
 """
 
 import math
-def going(n):
-	'''	
-	def inter(n):
-		if n == 1: 
-			return 1
-		else:
-			return 1 + 1.0 * (n-1) / n
-	'''
-	 
+def going(n):	 
 	result = 0
 	for i in range(n):
 		result = 1.0*result/(i+1) + 1
---
+++ where_my_anagrams.py
@@ -0,0 +1,10 @@
+'''
+	Where my anagrams at?
+	http://www.codewars.com/kata/523a86aa4230ebb5420001e1/train/python
+'''
+
+def anagrams(word, words):
+    #your code here
+
+    def test(aWord):
+
