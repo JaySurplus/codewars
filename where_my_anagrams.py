caa1a4a96f385532f5d55635a8e606988bdbff5e
JaySurplus <znznbest2004@gmail.com>
2015-08-06 23:55:21 -0500

    where my anagrams? Done.

--- where_my_anagrams.py
+++ where_my_anagrams.py
@@ -1,10 +1,17 @@
 '''
 	Where my anagrams at?
 	http://www.codewars.com/kata/523a86aa4230ebb5420001e1/train/python
+	Also could construct prime list, assign each character from word to a prime number. multiply them 
+	then divid prime number from word in words.
 '''
 
 def anagrams(word, words):
     #your code here
+    
+    return filter(lambda x: sorted(x) == sorted(word) , words)
+  
 
-    def test(aWord):
 
+print anagrams("thisis" , ["thisis", "isthis", "thisisis"])
+print anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer'])
+print anagrams('laser', ['lazing', 'lazy',  'lacer'])
