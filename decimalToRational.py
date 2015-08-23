172253328424b3459185981b7844ceab7332e280
JaySurplus <znznbest2004@gmail.com>
2015-08-22 21:43:06 -0500

    Modification on decimalToRational.py

--- decimalToRational.py
+++ decimalToRational.py
@@ -12,5 +12,4 @@ def converter(n, decimals=0, base=pi):
     """takes n in base 10 and returns it in any base (default is pi
     with optional x decimals"""
     #your code here
-    alphabeta = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     
