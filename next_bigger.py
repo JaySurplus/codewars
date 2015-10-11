1b1b48132b742b267862e953b81d6fef4df7d37d
JaySurplus <znznbest2004@gmail.com>
2015-10-11 04:16:12 -0500

    working on next_bigger.py

---
+++ next_bigger.py
@@ -0,0 +1,11 @@
+"""
+You have to create a function that takes a positive integer number and returns the next bigger number formed by the same digits:
+
+
+http://www.codewars.com/kata/55983863da40caa2c900004e/train/python
+
+"""
+
+def next_bigger(n):
+    #your code here
+    
